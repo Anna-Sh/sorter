38f5c9468649255daf9a6e8a6ffbca6b325730ec
Anna-Sh <a.shmidova@yandex.ru>
2018-09-23 19:33:00 +0300

    sorted

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
 
-  add(element) {
-    // your implementation
-  }
+    constructor() {
+        this.array = []; //инициализируем пустой массив, в котором будем хранить данные
+        this.comparatorFn = (a, b) => a - b //функция дефолтной сортировки для чисел
+    }
 
-  at(index) {
-    // your implementation
-  }
+    add(element) {
+        this.array.push(element); //добавляем элементы массива по одному
+    }
 
-  get length() {
-    // your implementation
-  }
+    at(index) {
+        return this.array[index]; //возвращаем элемент массива с индексом index
+    }
 
-  toArray() {
-    // your implementation
-  }
+    get length() {
+        return this.array.length; //получаем длину массива
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    toArray() {
+        return this.array; //возвращаем массив, без глубокой копии
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    sort(indices) {
+
+        indices.sort(); //сортируем выбранные индексы по возрастанию
+
+        const toSort = []; //инициализируем массив элементов, которые нуждаются в сортировке
+
+        for (let index of indices) {
+            toSort.push( this.array[index] ) //добавляем элементы
+        }
+
+        toSort.sort( this.comparatorFn ); //сортируем мини-массив функцией-компаратором
+
+        for (let i=0; i<toSort.length; i++) {
+            this.array.splice(indices[i], 1, toSort[i]) //меняем выбранные элемены массива,
+                                                        // расставляя их в соответствии с сортировкой
+        }
+
+    }
+
+    setComparator(compareFunction) {
+        this.comparatorFn = compareFunction //меняем дефолтную функцию-компаратор на выбранную пользователем
+    }
 }
+
 
 module.exports = Sorter;
